b61887216a2b417ae77029fecee39b8006a39730
Marc-Olivier Jufer <mjufer@ethz.ch>
2024-11-26 20:18:35 +0100

    Modified linking.sh for custom layout

--- linking.sh
+++ linking.sh
@@ -1,2 +1,11 @@
-ln -nfs /home/marco/dotfiles/.vimrc /home/marco/.vimrc
-cp -rs /home/marco/dotfiles/.config/ /home/marco/.config/
+# Backup current config files
+mv ~/.config ~/.config_backup
+mv ~/.vimrc ~/.vimrc_backup
+
+# Backup current ch layout
+mv /usr/share/X11/xkb/symbols/ch /usr/share/X11/xkb/symbols/ch_backup
+
+# Create symlinks for config files
+ln -nfs ./.vimrc ~/.vimrc
+cp -rs ./.config/ ~/.config/
+ln -nfs ./resources/ch /usr/share/X11/xkb/symbols/ch
