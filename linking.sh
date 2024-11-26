b4d768fddd1961cae2401ed9de774ce2ba279648
Marc-Olivier Jufer <mjufer@ethz.ch>
2024-11-26 21:38:52 +0100

    added copy option

--- linking.sh
+++ linking.sh
@@ -10,7 +10,14 @@ mv $HOME/.vimrc $HOME/.vimrc_backup
 # Backup current ch layout
 sudo mv /usr/share/X11/xkb/symbols/ch /usr/share/X11/xkb/symbols/ch_backup
 
-# Create symlinks for config files
-ln -nfs $CURRENT_DIR/.vimrc $HOME/.vimrc
-cp -rs $CURRENT_DIR/.config/ $HOME/.config/
-sudo ln -nfs $CURRENT_DIR/resources/ch /usr/share/X11/xkb/symbols/ch
+if [ "$1" == "--copy" ]; then
+  # Copy files in appropriates folders
+  cp $CURRENT_DIR/.vimrc $HOME/.vimrc
+  cp -r $CURRENT_DIR/.config/ $HOME/.config/
+  sudo cp $CURRENT_DIR/resources/ch /usr/share/X11/xkb/symbols/ch
+else
+  # Create symlinks for config files
+  ln -nfs $CURRENT_DIR/.vimrc $HOME/.vimrc
+  cp -rs $CURRENT_DIR/.config/ $HOME/.config/
+  sudo ln -nfs $CURRENT_DIR/resources/ch /usr/share/X11/xkb/symbols/ch
+fi
