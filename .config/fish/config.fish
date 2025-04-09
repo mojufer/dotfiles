465cd86a4ec0181ee6062d58b5a017ff3894cad9
Marc-Olivier Jufer <mjufer@ethz.ch>
2025-04-09 23:04:16 +0200

    some updates

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -2,7 +2,6 @@ if status is-interactive
     # Commands to run in interactive sessions can go here
 end
 
-
 # Aliases
 alias brave="brave --enable-features=UseOzonePlatform --ozone-platform=wayland"
 alias hssh="ssh -i /home/marco/.ssh/id_hezner_marco marco@188.245.36.178"
@@ -14,3 +13,5 @@ alias neofetch="fastfetch"
 alias vi="/bin/vim"
 alias nerd-font-installer='bash -c  "$(curl -fsSL https://raw.githubusercontent.com/officialrajdeepsingh/nerd-fonts-installer/main/install.sh)"'
 alias plan="zathura ~/mojufer/eth/Revisions/plan.pdf"
+
+thefuck --alias | source
